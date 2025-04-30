04070bba720413e3c297e33a49f2a53096f4c271
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2025-04-30 17:09:48 +0900

    fix header, bottom style

--- packages/user-ui/src/pages/projects/_id/discussion/_id/components/header.rs
+++ packages/user-ui/src/pages/projects/_id/discussion/_id/components/header.rs
@@ -7,20 +7,23 @@ use crate::components::icons::Logo;
 pub fn Header(title: String, onprev: EventHandler<MouseEvent>) -> Element {
     rsx! {
         div { class: "flex flex-row w-full justify-between items-center px-20 py-10 bg-netural-9",
-            div { class: "flex flex-row w-fit h-fit justify-start items-center gap-5",
+            div { class: "flex flex-row w-150 h-fit justify-start items-center gap-5",
                 Logo { width: "30", height: "20", class: "fill-third" }
                 div { class: "font-extrabold text-xs/15 text-third", "VOICE KOREA" }
             }
             div { class: "font-semibold text-lg/22 text-white", {title} }
-            button {
-                onclick: move |e: Event<MouseData>| {
-                    onprev.call(e);
-                },
-                Clear {
-                    width: "18",
-                    height: "18",
-                    fill: "#7c8292",
-                    class: "[&>path]:stroke-third",
+
+            div { class: "flex flex-row w-150 justify-end items-center",
+                button {
+                    onclick: move |e: Event<MouseData>| {
+                        onprev.call(e);
+                    },
+                    Clear {
+                        width: "18",
+                        height: "18",
+                        fill: "#7c8292",
+                        class: "[&>path]:stroke-third",
+                    }
                 }
             }
         }
