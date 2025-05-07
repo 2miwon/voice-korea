87ba79560e03cf817a6a717a60e2fcfcc1b7b276
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2025-05-07 10:47:50 +0900

    fix review pagination

--- packages/user-ui/src/pages/projects/components/sorter.rs
+++ packages/user-ui/src/pages/projects/components/sorter.rs
@@ -28,31 +28,29 @@ pub fn Sorter(
             },
 
             button { class: "flex flex-row w-190 focus:outline-none h-45 justify-between items-center bg-white border border-input-border-gray hover:bg-input-border-gray rounded-lg px-20",
-                div { class: "font-normal text-[#222222] text-[16px]", {sorter.translate(&lang)} }
+                div { class: "font-normal text-text-black text-base", {sorter.translate(&lang)} }
                 ChevronDown { color: "#555462", width: "18", height: "18" }
             }
 
-            if is_focused() {
-                div {
-                    class: "aria-active:hidden absolute top-full bg-white border border-input-border-gray shadow rounded-xl w-full h-200 z-50",
-                    "aria-active": !is_focused(),
-                    onclick: move |event| {
-                        event.stop_propagation();
-                        event.prevent_default();
-                    },
-                    div { class: "flex flex-col w-full justify-start items-start",
-                        div { class: "flex flex-col w-full justify-center items-center bg-transparent",
-                            for option in ProjectSorter::VARIANTS {
-                                div {
-                                    class: "flex flex-row w-full justify-center items-center hover:bg-input-border-gray py-15 cursor-pointer overflow-hidden",
-                                    role: "button",
-                                    onclick: move |_| {
-                                        on_sorter_changed.call(*option);
-                                        let prev = is_focused();
-                                        is_focused.set(!prev);
-                                    },
-                                    "{option.translate(&lang)}"
-                                }
+            div {
+                class: "aria-active:hidden absolute top-full bg-white border border-input-border-gray shadow rounded-xl w-full h-200 z-50",
+                "aria-active": !is_focused(),
+                onclick: move |event| {
+                    event.stop_propagation();
+                    event.prevent_default();
+                },
+                div { class: "flex flex-col w-full justify-start items-start",
+                    div { class: "flex flex-col w-full justify-center items-center bg-transparent",
+                        for option in ProjectSorter::VARIANTS {
+                            div {
+                                class: "flex flex-row w-full justify-center items-center hover:bg-input-border-gray py-15 cursor-pointer overflow-hidden",
+                                role: "button",
+                                onclick: move |_| {
+                                    on_sorter_changed.call(*option);
+                                    let prev = is_focused();
+                                    is_focused.set(!prev);
+                                },
+                                {option.translate(&lang)}
                             }
                         }
                     }
